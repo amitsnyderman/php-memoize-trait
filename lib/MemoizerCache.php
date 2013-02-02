77a5fbbb17223b0b045cd0b3ca4bd2e5ed1e8d58
Amit Snyderman <amit@amitsnyderman.com>
2013-02-02 04:50:11 -0500

    Cleanup cache interface

--- lib/MemoizerCache.php
+++ lib/MemoizerCache.php
@@ -1,18 +1,21 @@
 <?php
 
 interface MemoizerCache {
-	function get($key, $callback);
+	function get($key, callable $callback = null);
 	function set($key, $value);
 }
 
 class InMemoryMemoizerCache implements MemoizerCache {
 	private $cache = [];
 
-	public function get($key, $callback) {
+	public function get($key, callable $callback = null) {
 		if (array_key_exists($key, $this->cache)) {
 			return $this->cache[$key];
 		}
-		return $this->set($key, call_user_func($callback));
+		if (isset($callback)) {
+			return $this->set($key, call_user_func($callback));
+		}
+		return null;
 	}
 
 	public function set($key, $value) {
